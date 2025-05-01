9286cb99c2b53a7dfedb480f7aa77d2c112ee10b
Tom O'Brien <tobrien@nvidia.com>
2025-05-01 11:26:29 +0000

    wip

--- retriever/src/retriever/components/processor.py
+++ retriever/src/retriever/components/processor.py
@@ -13,42 +13,25 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import asyncio
+import json
 import logging
-import uuid
-from enum import Enum
-from typing import AsyncIterator, Tuple, Union
+from typing import Any
 
-from pydantic import BaseModel
-from retriever.components.trt_worker import TrtWorker
-from transformers import AutoTokenizer
+from retriever.common.chat_processor import ChatProcessorMixin
+from retriever.common.parser import parse_tensorrt_llm_args
 
-# from utils.chat_processor import ChatProcessor, CompletionsProcessor, ProcessMixIn
-# from utils.logging import check_required_workers
-# from utils.protocol import MyRequestOutput, Tokens, vLLMGenerateRequest
-# from utils.vllm import RouterType, parse_vllm_args
-from vllm.entrypoints.openai.protocol import ChatCompletionRequest, CompletionRequest
-from vllm.outputs import RequestOutput
-from vllm.transformers_utils.tokenizer import AnyTokenizer
+# from retriever.common.protocol import DynamoTRTLLMChatCompletionRequest
+from retriever.common.utils import RequestType
 
-from dynamo.sdk import async_on_start, depends, dynamo_context, dynamo_endpoint, service
-
-# from components.kv_router import Router
-# from components.worker import VllmWorker
+from dynamo.sdk import async_on_start, dynamo_context, dynamo_endpoint, service
 from dynamo.sdk.lib.config import ServiceConfig
 
+# from retriever.components.kv_router import Router
+# from retriever.components.worker import TensorRTLLMWorker
+
+
 logger = logging.getLogger(__name__)
-
-
-class RequestType(Enum):
-    CHAT = "chat"
-    COMPLETION = "completion"
-
-
-class ProcessorConfig(BaseModel):
-    """Configuration for the Frontend service including model and HTTP server settings."""
-
-    model: str
-    served_model_name: str
 
 
 @service(
@@ -59,47 +42,28 @@ class ProcessorConfig(BaseModel):
     resources={"cpu": "10", "memory": "20Gi"},
     workers=1,
 )
-class Processor:
-    """
-    vLLM pre and post processing
-    """
-
-    worker = depends(TrtWorker)
+class Processor(ChatProcessorMixin):
+    # worker = depends(TensorRTLLMWorker)
     # router = depends(Router)
 
-    def __init__(self):
+    def __init__(
+        self,
+    ):
+        class_name = self.__class__.__name__
         config = ServiceConfig.get_instance()
-        processor_config = ProcessorConfig.model_validate(
-            config.get(self.__class__.__name__, {})
-        )
-        # class_name = self.__class__.__name__
-        # self.engine_args = parse_vllm_args(class_name, "")
-        # self.model_config = self.engine_args.create_model_config()
-        self.tokenizer = self._create_tokenizer(model_path=processor_config.model)
-        # self.chat_processor = ChatProcessor(self.tokenizer, self.model_config)
-        # self.completions_processor = CompletionsProcessor(
-        #     self.tokenizer, self.model_config
-        # )
+        config_args = config.as_args(class_name, prefix="")
+        args, engine_config = parse_tensorrt_llm_args(config_args)
+        self.remote_prefill = args.remote_prefill
+        self.router_mode = args.router
         self.min_workers = 1
-        # print(f"Processor init: {self.engine_args.router}")
+        self.args = args
 
-    def _create_tokenizer(self, model_path: str) -> AnyTokenizer:
-        """Create a TokenizerGroup using engine arguments similar to VLLM's approach"""
-
-        # Create the base tokenizer with VLLM's typical settings
-        base_tokenizer = AutoTokenizer.from_pretrained(
-            model_path,
-            trust_remote_code=True,
-            padding_side="left",
-            truncation_side="left",
-            use_fast=True,  # VLLM might use the fast tokenizer for efficiency
-        )
-        return base_tokenizer
+        super().__init__(engine_config)
 
     @async_on_start
     async def async_init(self):
         runtime = dynamo_context["runtime"]
-        comp_ns, comp_name = TrtWorker.dynamo_address()  # type: ignore
+        comp_ns, comp_name = TensorRTLLMWorker.dynamo_address()  # type: ignore
         self.worker_client = (
             await runtime.namespace(comp_ns)
             .component(comp_name)
@@ -107,62 +71,40 @@ class Processor:
             .client()
         )
 
-        # if self.engine_args.router == RouterType.KV:
-        #     router_ns, router_name = Router.dynamo_address()  # type: ignore
-        #     self.router_client = (
-        #         await runtime.namespace(router_ns)
-        #         .component(router_name)
-        #         .endpoint("generate")
-        #         .client()
-        #     )
+        if self.args.router == "kv":
+            router_ns, router_name = Router.dynamo_address()  # type: ignore
+            self.router_client = (
+                await runtime.namespace(router_ns)
+                .component(router_name)
+                .endpoint("generate")
+                .client()
+            )
 
-        # await check_required_workers(self.worker_client, self.min_workers)
+        while len(self.worker_client.endpoint_ids()) < self.min_workers:
+            logger.info(
+                f"Waiting for workers to be ready.\n"
+                f" Current: {len(self.worker_client.endpoint_ids())},"
+                f" Required: {self.min_workers}"
+            )
+            await asyncio.sleep(30)
 
-        # # kv_listener = runtime.namespace("dynamo").component("TrtWorker")
-        # # await kv_listener.create_service()
-        # # self.metrics_aggregator = KvMetricsAggregator(kv_listener)
+    async def _generate(self, raw_request, request_type: RequestType):
+        raw_request.skip_special_tokens = False
+        raw_request.add_special_tokens = False
+        raw_request.spaces_between_special_tokens = False
+        logger.debug(f"[preprocessor] Received request: {raw_request}")
 
-        # self.etcd_kv_cache = await EtcdKvCache.create(
-        #     runtime.etcd_client(),
-        #     "/dynamo/processor/",
-        #     {"router": self.engine_args.router},
-        # )
+        if request_type == RequestType.CHAT:
+            preprocessed_request = await self.chat_processor.preprocess(raw_request)
+        else:
+            preprocessed_request = await self.completions_processor.preprocess(
+                raw_request
+            )
 
-    async def _get_kv_load(self):
-        metrics = await self.metrics_aggregator.get_metrics()
-        kv_load = {}
-        for endpoint in metrics.endpoints:
-            worker_id = endpoint.worker_id
-            kv_load[worker_id] = getattr(endpoint, "gpu_cache_usage_perc", 0.0)
-        return kv_load
-
-    async def _get_pending_requests(self):
-        metrics = await self.metrics_aggregator.get_metrics()
-        pending_requests = {}
-        for endpoint in metrics.endpoints:
-            worker_id = endpoint.worker_id
-            pending_requests[worker_id] = getattr(endpoint, "num_requests_waiting", 0)
-        return pending_requests
-
-    async def _generate(
-        self,
-        raw_request: Union[CompletionRequest, ChatCompletionRequest],
-        request_type: RequestType,
-    ):
-        request_id = str(uuid.uuid4())
-        logger.debug(f"Got raw request: {raw_request}")
-        (
-            request,
-            conversation,
-            prompt,
-            engine_prompt,
-            sampling_params,
-        ) = await self._parse_raw_request(raw_request)
-        # TODO: queue request at processor when engines are full
-        router_mode = (await self.etcd_kv_cache.get("router")).decode()
-        if router_mode == RouterType.KV:
+        worker_id = ""
+        if self.router_mode == "kv":
             router_generator = await self.router_client.generate(
-                Tokens(tokens=engine_prompt["prompt_token_ids"]).model_dump_json()
+                preprocessed_request.tokens.model_dump_json()
             )
             decision = await router_generator.__anext__()
             decision = decision.data()
@@ -172,111 +114,53 @@ class Processor:
                 f"Worker ID: {worker_id} with estimated prefix hit rate: {prefix_hit_rate}"
             )
 
-            if worker_id == "":
-                engine_generator = await self.worker_client.generate(
-                    vLLMGenerateRequest(
-                        engine_prompt=engine_prompt,
-                        sampling_params=sampling_params,
-                        request_id=request_id,
-                        prefix_hit_rate=prefix_hit_rate,
-                    ).model_dump_json()
-                )
+        if worker_id == "":
+            if self.router_mode == "round-robin":
+                self._send_request = self.worker_client.round_robin
             else:
-                engine_generator = await self.worker_client.direct(
-                    vLLMGenerateRequest(
-                        engine_prompt=engine_prompt,
-                        sampling_params=sampling_params,
-                        request_id=request_id,
-                        prefix_hit_rate=prefix_hit_rate,
-                    ).model_dump_json(),
-                    int(worker_id),
-                )
-        elif router_mode == RouterType.RANDOM:
-            engine_generator = await self.worker_client.generate(
-                vLLMGenerateRequest(
-                    engine_prompt=engine_prompt,
-                    sampling_params=sampling_params,
-                    request_id=request_id,
-                ).model_dump_json()
-            )
-        elif router_mode == RouterType.ROUND_ROBIN:
-            engine_generator = await self.worker_client.round_robin(
-                vLLMGenerateRequest(
-                    engine_prompt=engine_prompt,
-                    sampling_params=sampling_params,
-                    request_id=request_id,
-                ).model_dump_json()
-            )
-        elif router_mode == RouterType.KV_LOAD:
-            # route to worker with least kv load
-            # TODO: move the router to a separate file and clean up processor.py
-            try:
-                kv_load = await self._get_kv_load()
-                best_worker_id = min(kv_load, key=kv_load.get)
-                logger.info(f"Routing to worker {best_worker_id} (kv load: {kv_load})")
-                engine_generator = await self.worker_client.direct(
-                    vLLMGenerateRequest(
-                        engine_prompt=engine_prompt,
-                        sampling_params=sampling_params,
-                        request_id=request_id,
-                    ).model_dump_json(),
-                    int(best_worker_id),
-                )
-            except Exception as e:
-                logger.info(
-                    f"Error finding worker with least kv load: {e}, fallback to random"
-                )
-                engine_generator = await self.worker_client.generate(
-                    vLLMGenerateRequest(
-                        engine_prompt=engine_prompt,
-                        sampling_params=sampling_params,
-                        request_id=request_id,
-                    ).model_dump_json()
-                )
-        output = self._generate_responses(engine_generator, request_type)
+                # fallback to random
+                self._send_request = self.worker_client.random
 
-        async for response in await self._stream_response(
-            request, output, request_id, conversation
-        ):
-            yield response
-
-    async def _generate_responses(
-        self, engine_generator: AsyncIterator[RequestOutput], request_type: RequestType
-    ) -> AsyncIterator[Union[RequestOutput, Tuple[int, RequestOutput]]]:
-        prompt_idx = 0
-        async for resp in engine_generator:
-            # Deserialize the response from the engine
-            # Creates correct vLLM objects for each field
-            output = MyRequestOutput.model_validate_json(resp.data())
-
-            # OpenAIServingChat.chat_completion_stream_generator() method expects a RequestOutput object
-            request_output = RequestOutput(
-                request_id=output.request_id,
-                prompt=output.prompt,
-                prompt_token_ids=output.prompt_token_ids,
-                prompt_logprobs=output.prompt_logprobs,
-                outputs=output.outputs,
-                finished=output.finished,
-                metrics=output.metrics,
+            engine_generator = await self._send_request(
+                preprocessed_request.model_dump_json()
             )
 
-            if request_type == RequestType.CHAT:
-                # For chat requests, yield the request_output directly.
-                yield request_output
-            elif request_type == RequestType.COMPLETION:
-                # Completion requests can have multiple prompts and stream generator requires the prompt index
-                yield (prompt_idx, request_output)
+        else:
+            engine_generator = await self.worker_client.direct(
+                preprocessed_request.model_dump_json(), int(worker_id)
+            )
+
+        if request_type == RequestType.CHAT:
+            async for response in self.chat_processor.postprocess(
+                engine_generator,
+                raw_request,
+                preprocessed_request.conversation,
+            ):
+                logger.debug(f"[preprocessor] Response: {response}")
+                yield json.loads(response)
+        else:
+            async for response in self.completions_processor.postprocess(
+                engine_generator, raw_request
+            ):
+                logger.debug(f"[preprocessor] Response: {response}")
+                yield json.loads(response)
+
+    @dynamo_endpoint(name="chat/completions")
+    async def generate_chat(self, raw_request: Any):
+        # max_tokens is deprecated, however if the max_tokens is provided instead
+        # of max_completion_tokens, we will use the value as max_completion_tokens.
+        if raw_request.max_tokens is not None:
+            if raw_request.max_completion_tokens is None:
+                raw_request.max_completion_tokens = raw_request.max_tokens
             else:
-                raise NotImplementedError(
-                    f"Request type {request_type} not implemented"
-                )
-
-    @dynamo_endpoint(name="embeddings")
-    async def embeddings(self, raw_request: ChatCompletionRequest):
+                if raw_request.max_tokens != raw_request.max_completion_tokens:
+                    raise ValueError(
+                        "max_tokens and max_completion_tokens must be the same"
+                    )
         async for response in self._generate(raw_request, RequestType.CHAT):
             yield response
 
     # @dynamo_endpoint()
-    # async def completions(self, raw_request: CompletionRequest):
+    # async def completions(self, raw_request):
     #     async for response in self._generate(raw_request, RequestType.COMPLETION):
     #         yield response
